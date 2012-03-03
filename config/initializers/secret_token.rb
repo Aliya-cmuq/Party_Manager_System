6dfb6e1f73ee47c14c9af5616bd11ab77b6cb5e7
Aliya Hashim <ahashim@qatar.cmu.edu>
2012-03-03 18:22:07 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyManagerSystem::Application.config.secret_token = '5171073b37d0700153de69c67d9a6e68c81ac024b596b5dda60e0c3d4d3241ef831d3e07c87d147be4de462ae9fd930e1dfb2014045f5732c8ff98371d53119e'
